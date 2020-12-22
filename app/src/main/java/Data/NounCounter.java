e712eb24e33808b5127708c89b701117e09f2d1b
heymatch <match.remo@gmail.com>
2020-12-22 17:29:16 +0800

    Create NounCounter.java

---
+++ app/src/main/java/Data/NounCounter.java
@@ -0,0 +1,22 @@
+package Data;
+
+public class NounCounter {
+    private String noun;
+    private Integer counter;
+
+    public String getNoun() {
+        return noun;
+    }
+
+    public void setNoun(String noun) {
+        this.noun = noun;
+    }
+
+    public Integer getCounter() {
+        return counter;
+    }
+
+    public void setCounter(Integer counter) {
+        this.counter = counter;
+    }
+}
